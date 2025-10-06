53024fd8dc27b24e9b9d28a8fa7724b3291e4943
Steven Sum <steven.sum@drv-bund.de>
2025-10-06 10:19:32 +0200

    Skript passwort hashen

---
+++ Projekt/server/hash.js
@@ -0,0 +1,14 @@
+// hash.js - Hash-Generator für Passwort
+// Usage: node hash.js dein_passwort
+const bcrypt = require('bcrypt');
+
+const password = process.argv[2];
+if (!password) {
+  console.log('Bitte gib ein Passwort als Argument an!');
+  process.exit(1);
+}
+
+bcrypt.hash(password, 10).then(hash => {
+  console.log('Bcrypt-Hash für', password + ':');
+  console.log(hash);
+});
